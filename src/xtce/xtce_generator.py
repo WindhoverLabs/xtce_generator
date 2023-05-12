5bf19ce17cf06d2191aa156311f053075c58d5e4
Lorenzo Gomez <lgomez@windhoverlabs.com>
2023-05-12 17:30:31 -0500

    -Set parameter output name to output_name for algorithms.

--- src/xtce/xtce_generator.py
+++ src/xtce/xtce_generator.py
@@ -1915,7 +1915,7 @@ class XTCEManager:
              if aggregate_type and len(aggregate_type.get_MemberList().get_Member()) > 0:
                 if self.__aggregate_paramtype_exists(symbol[2], module_name) is False:
                     base_paramtype_set.add_AggregateParameterType(aggregate_type)
-                telemetry_param = xtce.ParameterType(name=parameter_ref,
+                telemetry_param = xtce.ParameterType(name=output_name,
                                                      parameterTypeRef=aggregate_type.get_name())
                 module_space_system.get_TelemetryMetaData().get_ParameterSet().add_Parameter(telemetry_param)
 
