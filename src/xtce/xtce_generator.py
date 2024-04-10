50d4ba03cb0722a5e798325438caccf0d9a56243
lgomez <lgomez@windhoverlabs.com>
2024-04-10 16:57:03 -0500

    -Set short description

--- src/xtce/xtce_generator.py
+++ src/xtce/xtce_generator.py
@@ -1625,7 +1625,7 @@ class XTCEManager:
             if min_rate is not None:
                 default_rate = xtce.RateInStreamType(minimumValue=min_rate)
 
-            seq_container = xtce.SequenceContainerType(name=str(tlm_name), DefaultRateInStream=default_rate, LongDescription=description)
+            seq_container = xtce.SequenceContainerType(name=str(tlm_name), DefaultRateInStream=default_rate, shortDescription=description)
             container_entry_list = xtce.EntryListType()
             seq_container.set_EntryList(container_entry_list)
 
